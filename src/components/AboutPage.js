7b01f11148c4a159e0c26af17258dd34fcc9048a
unknown <jominazhakathu@gmail.com>
2020-07-09 21:52:25 +0530

    initial cmmit

---
+++ src/components/AboutPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+    render() {
+        return (<>
+            <h1>AboutPage</h1>
+            <p>This app uses react.</p>
+        </>);
+
+    }
+}
+
+export default AboutPage;
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+    return (
+        < div className="jumbotron" >
+            <h1>PluralSight Administration</h1>
+            <p>React,flux and React Router fror ultra responsive web apps</p>
+            <a href="/about">about</a>
+        </div >
+    );
+}
+
+export default HomePage;
